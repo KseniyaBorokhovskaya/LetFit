0dc1e122141c15c09eec45fd0dc8636bbbb9b1e8
Kseniya Borokhovskaya <kseniyaborokhovskaya@Kseniyas-Mac.local>
2017-04-22 23:04:50 +0300

    Initial Commit

---
+++ FitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitApp
+//
+//  Created by Kseniya Borokhovskaya on 4/22/17.
+//  Copyright © 2017 Kseniya Borokhovskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
